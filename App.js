90cfbb55c90a88b63f12c00b472cd01983403713
Adriana Ávila <avilaapadriana@gmail.com>
2021-04-02 16:26:07 -0300

    primeira parte

--- App.js
+++ App.js
@@ -1,21 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, Button, Alert } from 'react-native';
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-export default function App() {
+
+const Stack = createStackNavigator();
+
+const MyStack = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen   //primeira tela
+          name="Principal"
+          component={TelaPrincipal}
+          options={{ title: 'Bem vindo a Tela Principal' }}
+        ></Stack.Screen>
+
+        <Stack.Screen  //segunda tela
+          name="Tela02"
+          component={Tela02}
+          options={{ title: 'Bem vindo a Tela 02' }}
+        ></Stack.Screen>        
+
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+const TelaPrincipal = ({ navigation }) => {
+  return (
+    <View>
+      <Button
+        title="Ir para Tela 02"
+        onPress = { () => navigation.navigate('Tela02', { name: 'Tela02' })}
+      ></Button>
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Tela02 = ({ navigation }) => {
+  return (
+    <View>
+      <Text>
+        Estou na Tela 02
+      </Text>
+    </View>
+  );
+};
+
+export default MyStack;
